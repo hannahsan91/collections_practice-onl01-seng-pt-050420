c010056528a885a41989be18feb65eab9ede310a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-14 00:35:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,12 @@ def sort_array_asc(sort_array)
   sort_array = [25, 7, 1]
   sort_array_asc.sort
 end
+
+def sort_array_desc(sort_array)
+  sort_array = [25, 7, 1]
+  sort_array_desc.sort
+end
+
+def sort_array_char_count
+  sort_array = [25, 7, 1]
+  sort_array_char_count.sort
